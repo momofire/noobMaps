3167793271d1387349c40363d2c5d8b2666fadad
Mohammad <momofire@gmail.com>
2011-10-01 21:12:32 -0400

    everything?

---
+++ script.js
@@ -0,0 +1,43 @@
+/* JavaScript goes here */
+
+jQuery(document).ready(function($) {
+  initializeForm();
+});
+
+function initializeForm() {
+  $("#address_form").submit(function(e){
+    $.ajax({
+      type: 'get',
+      datatype: 'json',
+      url: 'http://where.yahooapis.com/geocode',
+      data: { 
+        appid: '[yourappidhere]',
+        flags: 'J',
+        q: $("#address_input_field").val()
+      },
+      success: function(data, textStatus, jqXHR){        
+        lat = data.ResultSet.Results[0].latitude;
+        lng = data.ResultSet.Results[0].longitude;
+        
+        initializeMap(lat, lng);
+      },
+      error: function(e){
+        alert("AJAX call to API FAILED");
+      }
+    });
+    return false;
+  });
+};
+
+function initializeMap(lat, lng) {
+  lat = lat || -34.397;
+  lng = lng || 150.644;
+  
+  var myOptions = {
+    zoom: 14,
+    center: new google.maps.LatLng(lat, lng),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+}
